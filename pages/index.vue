a84ffb1655824520c06a952b6d5be2deff02f509
nicolaswalcker <walcker_@outlook.com>
2022-02-05 23:31:33 -0300

    feat: add structure on index

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,29 @@
 <template>
-  <Tutorial />
+  <main class="main">
+    <particles-container />
+    <section id="start"></section>
+    <section id="skills"></section>
+    <section id="works"></section>
+    <section id="about"></section>
+  </main>
 </template>
 
 <script>
+import ParticlesContainer from '~/components/ParticlesContainer.vue'
 export default {
   name: 'IndexPage',
+  components: { ParticlesContainer },
 }
 </script>
+<style lang="scss" scoped>
+.main {
+  background: $dark-200;
+  min-height: 100vh;
+  @include breakpoint(small-up) {
+    min-height: calc(100vh - 85px);
+  }
+}
+section {
+  height: 100%;
+}
+</style>
